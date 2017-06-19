b5c558470ba2a1639d48897defaff348c67f9c72
Coto Cisternas <cotocisternas@gmail.com>
2017-06-19 16:13:22 -0400

    fix ens3?

--- debian/scripts/fix-grub.sh
+++ debian/scripts/fix-grub.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet net.ifnames=0 biosdevname=0 kvm-intel.nested=1"/g' /etc/default/grub
 /usr/sbin/update-grub2
+sed -i 's/ens3/eth0/g' /etc/network/interfaces
